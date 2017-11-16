0e245aeb6aecdb7b940cedd62ea110e48059f42b
Дмитрий Жиляев <zhilyaev@users.noreply.github.com>
2017-11-17 00:06:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,25 +10,44 @@ G = int(sys.argv[2])  # Группа
 print("Вариант: "+str(V))
 print("Группа: "+str(G))
 S = V + G
-x1 = (-1 ** V) * (S * 3)
-x2 = (-1 ** (V + 1)) * (S + 17)
-x3 = (-1 ** (V + 2)) * ((S + 29) ** 2)
-x4 = (-1 ** (V + 3)) * ((S + 23) ** 2)
+print("Вариант: " + str(V))
+print("Группа: " + str(G))
+S = V + G
+x1 = ((-1) ** V) * (S * 3)
+x2 = ((-1) ** (V + 1)) * (S + 17)
+x3 = ((-1) ** (V + 2)) * ((S + 29) ** 2)
+x4 = ((-1) ** (V + 3)) * ((S + 23) ** 2)
 x5 = x3 ** 2
-x6 = -1 * (x4 ** 2)
-x7 = -1 * (x5 * (2 ** 28))
-x8 = -1 * (x6 * (2 ** 20))
+x6 = (-1) * (x4 ** 2)
+x7 = (-1) * (x5 * (2 ** 28))
+x8 = (-1) * (x6 * (2 ** 20))
 x9 = (x7 * (2 ** 52)) - 0xc
 x = [x1, x2, x3, x4, x5, x6, x7, x8, x9]
 print()
-print("DECIMAL :")
+print("Пункт A) Десятичные :")
 for i in x:
     print(i)
 print()
-print("HEX :")
+print("Пункт Б) Перевод в HEX :")
 for i in x:
-    print(toHex(i, 128))
+    print(toHex(i, BIT))
 print()
-print("ADDRESS :")
+print("Пункт Г) Адресса данных :")
 for i in range(len(x)):
-    print(S + i * 10)
+    it = (G * V) + 10 * i
+    print(str(it) + " => " + toHex(it, BIT))  # toHex() can be replaced with hex()
+print()
+print("Пункт Д) Адресса данных :")
+for i in range(len(x)):
+    it = V + 100 + (i * 10)
+    print(str(it) + " => " + toHex(it, BIT))  # toHex() can be replaced with hex()
+print()
+print("E) Начальный адрес размещения:")
+print(V * 10 + 200)
+print()
+print("Ж) Начальный адрес размещения:")
+print(V * G + 230)
+print()
+print("З) Начальный адрес размещения:")
+print(V * G + 300)
+print()
